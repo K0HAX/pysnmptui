da6234372a09d4cbaa225f5265afca1468304810
Michael Englehorn <michael@englehorn.com>
2022-11-27 23:05:20 -0600

    Bumped to v0.1.2022112704, major refactoring

--- src/pysnmptui_K0HAX/__init__.py
+++ src/pysnmptui_K0HAX/__init__.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-from .snmp import SnmpUI, Client
+from .snmp import SnmpUI
 import argparse
 import sys
+
+def NotApplicationError(Exception):
+    pass
 
 def Exit():
     sys.exit(0)
@@ -11,40 +14,17 @@ def main(
         userName,
         authPass,
         privPass,
-        version=3
+        version=3,
+        extraMibs=[]
         ):
-    myUI = SnmpUI()
-    myUI.setSNMP(
-            host=host,
-            version=version,
-            userName=userName,
-            authPass=authPass,
-            privPass=privPass
-            )
+    myUI = SnmpUI(
+        host,
+        version,
+        userName,
+        authPass,
+        privPass,
+        extraMibs)
     myUI.run()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-            prog = "snmpui",
-            description = "Python SNMP TUI (Terminal User Interface)"
-            )
-    parser.add_argument('-H', '--host', required=True, help="SNMP Host")
-    parser.add_argument('-u', '--user', required=True, help="SNMP Username")
-    parser.add_argument('-p', '--authpass', required=True, help="SNMP Auth Password")
-    parser.add_argument('-x', '--privpass', required=True, help="SNMP Encryption (priv) Password")
-    parser.add_argument('-v', '--version', choices=["3"], default="3", help="SNMP Version")
-    args = parser.parse_args()
-    host = args.host
-    userName = args.user
-    authPass = args.authpass
-    privPass = args.privpass
-    version = args.version
-    if version == "3":
-        version = 3
-    main(
-            host,
-            userName,
-            authPass,
-            privPass,
-            version
-            )
+    raise NotApplicationError
